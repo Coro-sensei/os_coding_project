53877e3db66757f711f631a461ca324e26c6d36b
deelicieuse <libatiqueangelique@gmail.com>
2026-06-21 15:53:22 +0800

    adding fcfs algo

--- mass_storage_management/mass_storage.py
+++ mass_storage_management/mass_storage.py
@@ -3,3 +3,7 @@ from tkinter import ttk, messagebox
 
 def calc_movement(path):
     return sum(abs(path[i] - path[i+1]) for i in range(len(path) - 1))
+
+def calculate_fcfs(queue, head):
+    path = [head] + queue
+    return path, calc_movement(path)
